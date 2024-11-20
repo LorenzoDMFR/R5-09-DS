732bdac38a6cdedad7659c71321b128ac3984fa1
LorenzoDMFR <130384298+LorenzoDMFR@users.noreply.github.com>
2024-11-20 14:59:08 +0100

    Remplacement de * en X Q5.java

--- Q5.java
+++ Q5.java
@@ -5,7 +5,7 @@ public class Q5{
         int n = Integer.parseInt(args[0]);
         for(int i = 0; i < n; i++){
             for(int j = n; j > i; j--){
-                System.out.print("*");
+                System.out.print("X");
             }
             System.out.println();
         }
