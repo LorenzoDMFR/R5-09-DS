11ec0b2eabb1fc765e6e996bf12e8a3b6df541a8
LorenzoDMFR <130384298+LorenzoDMFR@users.noreply.github.com>
2024-11-20 14:25:35 +0100

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,5 +1,5 @@
+// fichier Q3.java, qui affiche une ligne d’étoiles ‘*’. Le nombre d’étoiles est donné en paramètres
 public class Q3{
-    // fichier Q3.java, qui affiche une ligne d’étoiles ‘*’. Le nombre d’étoiles est donné en paramètres
     public static void main(String[] args){
         int n = Integer.parseInt(args[0]);
         for(int i = 0; i < n; i++){
